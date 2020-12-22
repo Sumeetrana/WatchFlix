8fa54e78212d278d83ebcd1f1fc2c3fa9e05eda5
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-22 13:41:30 +0530

    feat: making jumbo compound component#1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
+import jumbotronData from "./fixtures/jumbotron.json";
 import Jumbo from "./components/jumbo";
 
 export default function App() {
-  return <Jumbo />;
+  return (
+    <Jumbo.Container>
+      {jumbotronData.map((item) => (
+        <Jumbo key={item.id} direction={item.direction}>
+          <Jumbo.Pane>
+            <Jumbo.Title>{item.title}</Jumbo.Title>
+            <Jumbo.SubTitle>{item.subTitle}</Jumbo.SubTitle>
+          </Jumbo.Pane>
+          <Jumbo.Pane>
+            <Jumbo.Image src={item.image} alt={item.alt} />
+          </Jumbo.Pane>
+        </Jumbo>
+      ))}
+    </Jumbo.Container>
+  );
 }
