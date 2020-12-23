f0e258797c1d1cd62f88c083d97b772de09b2e6d
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-23 11:17:22 +0530

    feat: footer container complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { FooterContiner } from "./containers/footer";
 import { JumboContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return <JumboContainer />;
+  return (
+    <>
+      <JumboContainer />
+      <FooterContiner />
+    </>
+  );
 }
