d199d72061277d2a947840845128f2968a68797c
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-24 10:10:12 +0530

    feat: react-router added for home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { AccordionContainer } from "./containers/accordions";
 import { FooterContiner } from "./containers/footer";
 import { JumboContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
   return (
-    <>
-      <JumboContainer />
-      <AccordionContainer />
-      <FooterContiner />
-    </>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <JumboContainer />
+        <AccordionContainer />
+        <FooterContiner />
+      </Route>
+    </Router>
   );
 }
