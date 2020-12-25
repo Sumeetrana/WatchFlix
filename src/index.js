e9f040b36c63d1642ea9ef0e345eb10a03d187eb
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-25 10:26:11 +0530

    feat: react context for firebase

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import { GlobalStyles } from "./global-styles";
-import {firebase} from "./lib/firebase.prod";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 import App from "./app";
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
