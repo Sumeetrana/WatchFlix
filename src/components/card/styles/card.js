be40a7e772b66c900754d616cf02d2335524d707
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-27 21:09:26 +0530

    feat: card component

---
+++ src/components/card/styles/card.js
@@ -0,0 +1,29 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div``;
+
+export const Title = styled.p``;
+
+export const Group = styled.div``;
+
+export const SubTitle = styled.p;
+
+export const Text = styled.p;
+
+export const Item = styled.p;
+
+export const Feature = styled.div``;
+
+export const FeatureText = styled.p``;
+
+export const FeatureTitle = styled(Title)``;
+
+export const FeatureClose = styled.button``;
+
+export const Maturity = styled.div``;
+
+export const Context = styled.div``;
+
+export const Meta = styled.div``;
+
+export const Image = styled.img``;
