3f02b35c0b1f9b91699a26c54520a244b219100b
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-27 21:34:26 +0530

    feat: Card list(categories, slides)-films & series logic#2

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -80,7 +80,13 @@ export function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
-      <Card.Group></Card.Group>
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+          </Card>
+        ))}
+      </Card.Group>
     </>
   ) : (
     <SelectionProfileContainer user={user} setProfile={setProfile} />
